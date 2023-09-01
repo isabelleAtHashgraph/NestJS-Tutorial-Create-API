6832d027420963e6557c82944e84e2c9264afc94
Isabelle <isabelle@gmail.com>
2023-09-01 15:21:01 +0200

    feat: add authguard and validation

---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,22 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(config: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: config.get('JWT_SECRET'),
+    });
+  }
+
+  async validate(payload: any) {
+    console.log(
+      '🚀 ~ file: jwt.strategy.ts:16 ~ JwtStrategy ~ validate ~ payload:',
+      payload,
+    );
+    return payload;
+  }
+}
